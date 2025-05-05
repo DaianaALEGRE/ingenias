ec7f92866f56ab35750be5ccdf6acbb9189b8e2d
Daiana Alegre <daianayanilalegre@gmail.com>
2025-05-05 17:20:31 -0300

    solucionar categoria

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ app.get('/', (req, res) => {
     res.status(200)
 
 });
-
+//catalogo
 
 app.get('/catalogo', (req, res) => {
 
-    res.json(dataObj);
+    res.json(trailerflix);
 
-
+//titulo
 }); app.get('/titulo/:title', (req, res) => {
     const tituloSolicitado = req.params.title.toLowerCase();
     const resultadoT = trailerflix.filter(p => p.titulo.includes(tituloSolicitado))
@@ -45,24 +45,28 @@ app.get('/catalogo', (req, res) => {
     else {
         res.json(resultadoT);
     }
-}); app.get('/categoria/:cat', (req, res) => {
+});//catergoria
+
+app.get('/categoria/:cat', (req, res) => {
     //filter
-    const categoria = req.params.cat.toLocaleLowerCase();
+     const categoria = req.params.cat.toLowerCase().trim();
     const resultadoC = trailerflix.filter(c => c.categoria.includes(categoria));
-    if (!resultadoC) {
+    if (resultadoC.length === 0) {
         return res.status(404).json({ mesage: 'No hay resultados para tu busqueda' });
     } else { res.json(resultadoC); }
 
+    //Reparto
+//validaciones cuando no encuentra q no tire error por lowcase
 
 }); app.get('/reparto/:act', (req, res) => {
-    const reparto = req.params.act.toLowerCase();
+    const reparto = req.params.act.toLowerCase().trim();
     const resultado = trailerflix.filter(r => r.reparto && r.reparto.includes(reparto));
 
 
     if (resultado.length === 0) {
         return res.status(404).json({ mesage: 'no se encontro coincidencias' })
     } else {
-        mapeado = resultado.map(({ titulo, reparto }) => ({
+        const mapeado = resultado.map(({ titulo, reparto }) => ({
             titulo, reparto
         }))
 
@@ -70,20 +74,20 @@ app.get('/catalogo', (req, res) => {
         res.json(mapeado)
     };
 
-
+// trailer
 }); app.get('/trailer/:id', (req, res) => {
-    const trailers = Number(req.params.id); // nombre tal como usaste antes
+    const trailers = Number(req.params.id); 
 
     if (isNaN(trailers)) {
         return res.status(400).json({ message: 'ID inválido. Debe ser un número.' });
     }
 
-    const trailerId = trailerflix.find(p => p.id === trailers); // p en vez de I o nombre más técnico
+    const trailerId = trailerflix.find(p => p.id === trailers); 
 
     if (!trailerId) {
         return res.status(404).json({ mesage: 'no se encontro coincidencias' });
     }
-
+//trailer por id y mapeado para q solo devuelva titulo y trailer(despues lo voy a desestructurar)
     if (!trailerId.trailer) {
         return res.json({
             id: trailerId.id,
